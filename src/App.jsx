b69af5644d674430fecda036d1fc0f0ae2adaf5f
rahul <rahulraazrs123@gmail.com>
2025-06-09 09:51:57 +0545

    Userdashboard Frontend Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
+import { BrowserRouter as Router , Routes, Route } from 'react-router-dom';
 import Freelancerprofile from './components/Freelancerprofile.jsx'
 import Freelancerdetails from './components/Freelancerdetails.jsx'
-import { BrowserRouter as Router , Routes, Route } from 'react-router-dom';
+import Userdashboard from './components/Userdashboard.jsx'
+
+
+
+
+
+
 export default function App(){
     return(<>
     <Router>
         <Routes>
             <Route path='/' element={<Freelancerdetails/>}></Route>
         </Routes>
+
         <Routes>
-            <Route path='/profilepage' element={<Freelancerprofile/>}></Route>
+            <Route path='/freelancerprofile' element={<Freelancerprofile/>}></Route>
+        </Routes>
+
+        <Routes>
+            <Route path='/userdashboard' element={<Userdashboard/>}></Route>
         </Routes>
     </Router>
     </>
